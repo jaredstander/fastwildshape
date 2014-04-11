b08d4dacfeb86006cbb16b85e61444a0e2902264
jaredstander <jaredstander@gmail.com>
2014-04-11 18:40:56 -0400

    Adding rails files to project

--- README.md
+++
@@ -1,4 +0,0 @@
-fastwildshape
-=============
-
-Rails Project for managing characters with the Wild Shape ability for the Pathfinder OGL tabletop gaming system.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fastwildshape::Application.config.secret_key_base = '96add7bcaaf0ac4ec4660eb3cc74b6453c97c15c76d7f0733de6412c5da3e03dc4a168ccb17de7c99178ff7b77f188f3bec5d289cba1fb087d247e1e75d70da7'
