ec4448855a5ed3f52d75afa628dc74842ee8ac7a
jaredstander <jaredstander@gmail.com>
2014-04-23 18:53:53 -0400

    Reformmating some user model tests for simplicity, re: presence and formatting.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,24 @@
 require "spec_helper"
 
 describe User do
-  context "#new" do
-    it "allows mass assignment of email" do
-      expect { user = User.new(email: "knightartorias@lordran.com") }.not_to raise_error
+
+  it { should have_db_index(:name) }
+  it { should have_db_index(:email) }
+  it { should have_db_index(:password) }
+
+  describe "when creating a new user" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    # email only validates presence and inclusion of @ symbol
+    # email to user with confirmation link will weed more complex but invalid email addresses
+    it { should allow_value("e@e").for(:email) }
+    it { should_not allow_value("emailatexample.com").for(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+    it "stores all emails as downcase with white space truncated" do
+      user = create(:user, email: " Jo hn.Do e @exa mp le.c om ")
+      user.email.should == "john.doe@example.com"
     end
-
-    it "allows mass assignment of name" do
-      expect { user = User.new(name: "Knight Artorias") }.not_to raise_error
-    end
-
-    it "allows mass assignment of password" do
-      expect { user = User.new(password: "AGreatPassword!") }.not_to raise_error
-    end
-
-    it "allows mass assignment of password_confirmation" do
-      expect { user = User.new(password_confirmation: "AGreatPassword!") }.not_to raise_error
-
-    it { should respond_to(email) }
-    it { should respond_to(name) }
-    it { should respond_to(password) }
-    it { should respond_to(password_confirmation) }
   end
 
   context "#save" do
