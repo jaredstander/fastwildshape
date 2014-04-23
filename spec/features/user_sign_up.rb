0b1d469007d405c4411d433e39d1269850fbe2b6
jaredstander <jaredstander@gmail.com>
2014-04-23 18:17:17 -0400

    Adding user sign up integration tests using rspec features specs.

--- spec/features/user_sign_up.rb
+++ spec/features/user_sign_up.rb
@@ -5,5 +5,25 @@ feature "user sign up" do
     visit root_url
   end
 
-  
+  let(user) { create(:user) }
+  subject { page }
+
+  it { should have_link ("Sign Up") }
+
+  scenario "sign up link navigates to user creation page" do
+    click_link("Sign Up")
+    expect(current_path).to eq new_user_path
+  end
+
+  scenario "sign up page has form elements: name, email, password, password confirmation, submit button" do
+    visit new_user_path
+    expect(page).to have_field("Name")
+    expect(page).to have_field("Email")
+    expect(page).to have_field("Password")
+    expect(page).to have_field("PasswordConfirmation")
+  end
+
+  scenario "user can fill in form fields with content" do
+    visit new_user_path
+    
 end
