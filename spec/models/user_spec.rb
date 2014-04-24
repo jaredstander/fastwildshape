12ecc187101efde1c4a2d0a22c3b0ea346dc6ff0
jaredstander <jaredstander@gmail.com>
2014-04-23 20:11:30 -0400

    Deleted old test text for validating presence of.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,24 +21,4 @@ describe User do
     end
   end
 
-  context "#save" do
-    it "requires an email address" do
-      user = User.new(name: "Knight Artorias", password: "AGreatPassword!", password_confirmation: "AGreatPassword!")
-      expect(user).not_to be_valid
-    end
-
-    it "requires a name" do
-      user = User.new(email: "knightartorias@lordran.com", password: "AGreatPassword!", password_confirmation: "AGreatPassword!")
-      expect(user).not_to be_valid
-    end
-
-    it "requires a password" do
-      user = User.new(email: "knightartorias@lordran.com", name: "Knight Artorias", password_confirmation: "AGreatPassword!")
-      expect(user).not_to be_valid
-    end
-
-    it "requires passwords to match" do
-      user = User.new(name: "Knight Artorias", email: "knightartorias@lordran.com", password: "AGreatPassword!", password_confirmation: "NotTheSamePassword!")
-      expect(user).not_to be_valid
-  end
 end
