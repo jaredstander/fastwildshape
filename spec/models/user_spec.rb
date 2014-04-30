12efead894a1d32a2f5ac0fc0bdc101cb99f1647
jaredstander <jaredstander@gmail.com>
2014-04-30 19:36:31 -0400

    Continuing to add unit tests, revising existing ones.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,16 +9,40 @@ describe User do
   describe "when creating a new user" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
-    # email only validates presence and inclusion of @ symbol
-    # email to user with confirmation link will weed more complex but invalid email addresses
+    # email only validates presence, and inclusion of @ symbol
+    # will email user with confirmation link, which will be final validation
     it { should allow_value("e@e").for(:email) }
     it { should_not allow_value("emailatexample.com").for(:email) }
-    it { should validate_presence_of(:password) }
-    it { should validate_presence_of(:password_confirmation) }
     it "stores all emails as downcase with white space truncated" do
       user = create(:user, email: " Jo hn.Do e @exa mp le.c om ")
       user.email.should == "john.doe@example.com"
     end
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+    it "Should ensure passwords match" do
+      user = create(name: "Knight Artorias", email: "knightartorias@lordran.com", password: "AGreatPassword!", password_confirmation: "AGreatPassword!")
+      user.password.should eq(password_confirmation)
+    end
+    it { should ensure_length_of(:password).is_at_least(8) }
+    it { should_not allow_mass_assignment_of(:password) }
   end
 
+  describe "user activities" do
+    before do
+      user = create(name: "Knight Artorias", email: "knightartorias@lordran.com", password: "AGreatPassword!", password_confirmation: "AGreatPassword!")
+    end
+
+    it "authenticates with a correct email and password" do
+      user.authenticate(user.email, user.password).should eq(user)
+    end
+
+    it "authenticates with a correct email and password regardless of email case" do
+      user.authenticate(user.email.uppercase, user.password).should eq(user)
+    end
+
+    it "should not authenticate with an incorrect email or password" do
+      user.authenticate(user.email, "WrongPassword").should be_nil
+      user.authenticate("randomemail@account.com", user.password).should be_nil
+    end
+  end
 end
