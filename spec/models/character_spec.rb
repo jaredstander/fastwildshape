791c1d82ed1de72c1aa9e6e6669abcb426b6ef05
jaredstander <jaredstander@gmail.com>
2014-05-15 16:42:49 -0400

    Building base unit tests for character model.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -2,8 +2,21 @@ require "spec_helper"
 
 describe Character do
   
-  it { should_have_db_index(:name) }
-  it { should_have_db_index(:druid?) }
-  it { should_have_db_index(:level) }
+  it { should have_db_index(:character_name) }
+  it { should have_db_index(:druid?) }
+  it { should have_db_index(:druid_archtype) }
+  it { should have_db_index(:level) }
+
+  describe "when creating a new character" do
+    it { should validate_presence_of(:character_name) }
+    it { should validate_presence_of(:druid?) }
+    it { should validate_presence_of(:level) }
+    it { should validate_uniqueness_of(:character_name) }
+    it "stores druid? as a boolean value" do
+      it { should allow_value(true).for(:druid?) }
+      it { should allow_value(false).for(:druid?) }
+    end
+
+    
 
 end
