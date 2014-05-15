ea14a26c552e54e8fc78240e015f8b9779234be0
jaredstander <jaredstander@gmail.com>
2014-05-15 17:19:31 -0400

    Updates to initial character unit tests.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -5,18 +5,25 @@ describe Character do
   it { should have_db_index(:character_name) }
   it { should have_db_index(:druid?) }
   it { should have_db_index(:druid_archtype) }
+  it { should have_db_index(:archtype_level_adjustment) }
+  it { should have_db_index(:archtype_type_adjustment) }
   it { should have_db_index(:level) }
+
 
   describe "when creating a new character" do
     it { should validate_presence_of(:character_name) }
     it { should validate_presence_of(:druid?) }
     it { should validate_presence_of(:level) }
     it { should validate_uniqueness_of(:character_name) }
+    it { should allow_mass_asignment_of(:character_name) }
+    it { should allow_mass_asignment_of(:druid?) }
+    it { should allow_mass_asignment_of(:druid_archtype) }
+    it { should allow_mass_asignment_of(:level) }
     it "stores druid? as a boolean value" do
       it { should allow_value(true).for(:druid?) }
       it { should allow_value(false).for(:druid?) }
     end
-
-    
+    it { should validate_numericality_of(:level).is_greater_than(0).is_less_than(21) }
+  end
 
 end
