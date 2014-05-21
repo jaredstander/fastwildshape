1a1aff7605c1915e3c9e78c6a0d53df4c791f5cf
jaredstander <jaredstander@gmail.com>
2014-05-21 18:09:56 -0400

    More character unit tests; adding required fields.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -3,17 +3,26 @@ require "spec_helper"
 describe Character do
   
   it { should have_db_index(:character_name) }
+  it { should have_db_index(:character_size) }
   it { should have_db_index(:druid?) }
   it { should have_db_index(:druid_archtype) }
   it { should have_db_index(:archtype_level_adjustment) }
   it { should have_db_index(:archtype_type_adjustment) }
   it { should have_db_index(:level) }
-
+  it { should have_db_index(:strength) }
+  it { should have_db_index(:dexterity) }
+  it { should have_db_index(:constitution) }
+  it { should have_db_index(:base_attack_bonus) }
+  it { should have_db_index(:total_ac) }
+  it { should have_db_index(:touch_ac) }
+  it { should have_db_index(:flatfooted_ac) }
+  it { should have_db_index(:stackable_ac) }
 
   describe "when creating a new character" do
     it { should validate_presence_of(:character_name) }
     it { should validate_presence_of(:druid?) }
     it { should validate_presence_of(:level) }
+    # Archtype is optional; present a dropdown list, and have "None" as the default selection
     it { should validate_uniqueness_of(:character_name) }
     it { should allow_mass_asignment_of(:character_name) }
     it { should allow_mass_asignment_of(:druid?) }
