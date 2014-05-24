ac6687da425061f6cebbd3de23864d889a556c1d
jaredstander <jaredstander@gmail.com>
2014-05-24 17:51:26 -0400

    Created character factory. Subject to changes as test evolves.

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -4,20 +4,20 @@ FactoryGirl.define do
     name { "#{Faker::Name.name}" }
     character_name { "Zaiko" }
     character_size { "Medium" }
-    it { should have_db_index(:level) } # integer 1-20
-    it { should have_db_index(:strength) } # integer 1-30
-    it { should have_db_index(:dexterity) } # integer 1-30
-    it { should have_db_index(:constitution) } # integer 1-30
-    it { should have_db_index(:base_attack_bonus) } # integer 1-20
-    it { should have_db_index(:total_ac) } # integer, 10 and up
-    it { should have_db_index(:touch_ac) } # integer, 10 and up
-    it { should have_db_index(:flatfooted_ac) } # integer, 10 and up
-    it { should have_db_index(:stackable_ac) } # integer, 0 and up
-    it { should have_db_index(:druid?) } # checkbox or radio buttons
-    it { should have_db_index(:has_druid_archtype?) } # determined by archtype dropdown choice
-    it { should have_db_index(:druid_archtype) } # dropdown choice
-    it { should have_db_index(:archtype_level_adjustment) } # algorithm
-    it { should have_db_index(:archtype_type_adjustment) } # algorithm
+    level { 4 }
+    strength { 12 }
+    dexterity { 12 }
+    constitution { 10 }
+    base_attack_bonus { 3 }
+    total_ac { 16 }
+    touch_ac { 12 }
+    flatfooted_ac { 14 }
+    stackable_ac { 0 }
+    druid? { true }
+    should have_db_index(:has_druid_archtype? { false }
+    druid_archtype { "none" }
+    archtype_level_adjustment { 0 }
+    archtype_type_adjustment {"none" }
   end
 
 end
